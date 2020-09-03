0aec524c95fccf8d7c6ecf3941578d8ee0dcd8d2
妖雨纯 <lisfes@yaoyuchundeMacBook-Pro.local>
2020-09-03 22:22:57 +0800

    样式修复

--- src/components/common/NavigaTion.vue
+++ src/components/common/NavigaTion.vue
@@ -1,63 +1,76 @@
 <script>
-import { NavBar,Icon,Tab,Tabs } from 'vant';
-import play from '@assets/icon/play.gif';
-import stop from '@assets/icon/stop.png';
+import { NavBar, Icon, Tab, Tabs } from 'vant'
+import play from '@assets/icon/play.gif'
+import stop from '@assets/icon/stop.png'
 export default {
-    name: 'NavigaTion',
-    props: {
-        active: {
-            type: Number,
-            default: 0
-        },
-        play: {
-            type: Boolean,
-            default: false
-        }
-    },
-    render() {
-        return (
-            <NavBar
-                class="NavigaTion"
-                onClick-left={() => {this.$emit('search')}}
-                onClick-right={() => {this.$emit('player')}}
-            >
-                <Icon
-                    slot="left"
-                    name="search"
-                    color="#ee0a24"
-                    size={20}
-                    style={{cursor: 'pointer'}}
-                ></Icon>
-                <div slot="title" class="Center">
-                    <Tabs type="card" active={this.active} onChange={(index) => {this.$emit('change', index)}}>
-                        <Tab title="推荐"></Tab>
-                        <Tab title="我的"></Tab>
-                        <Tab title="歌手"></Tab>
-                    </Tabs>
-                </div>
-                <Icon
-                    slot="right"
-                    name={this.play ? play : stop}
-                    size={20}
-                    style={{cursor: 'pointer'}}
-                ></Icon>
-            </NavBar>
-        )
-    }
+	name: 'NavigaTion',
+	props: {
+		active: {
+			type: Number,
+			default: 0
+		},
+		play: {
+			type: Boolean,
+			default: false
+		}
+	},
+	render() {
+		return (
+			<NavBar
+				class="NavigaTion"
+				onClick-left={() => {
+					this.$emit('search')
+				}}
+				onClick-right={() => {
+					this.$emit('player')
+				}}
+			>
+				<Icon
+					slot="left"
+					name="search"
+					color="#ee0a24"
+					size={20}
+					style={{ cursor: 'pointer' }}
+				></Icon>
+				<div slot="title" class="Center">
+					<Tabs
+						type="card"
+						active={this.active}
+						onChange={index => {
+							this.$emit('change', index)
+						}}
+					>
+						<Tab title="推荐"></Tab>
+						<Tab title="我的"></Tab>
+						<Tab title="歌手"></Tab>
+					</Tabs>
+				</div>
+				<Icon
+					slot="right"
+					name={this.play ? play : stop}
+					size={20}
+					style={{ cursor: 'pointer' }}
+				></Icon>
+			</NavBar>
+		)
+	}
 }
 </script>
 
 <style lang="less">
 .NavigaTion {
-    .Center {
-        height: 46PX;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        .van-tabs__nav--card {
-            margin: 0;
-        }
-    }
-    
+	.van-nav-bar__title {
+		width: 60%;
+	}
+	.Center {
+		/*prettier-ignore*/
+		height: 46PX;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		.van-tabs__nav--card {
+			margin: 0;
+		}
+	}
 }
 </style>
